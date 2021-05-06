093d3fc2a22d2dd715925f935c2deff7d1cfc262
pavelexpertov <pavelm01@hotmail.co.uk>
2021-05-06 14:09:52 +0100

    Add some code for square matrix

--- linear_algebra_by_j_borwnlee/ch_10/scribble_of_different_types_of_matrices.py
+++ linear_algebra_by_j_borwnlee/ch_10/scribble_of_different_types_of_matrices.py
@@ -5,17 +5,33 @@ import numpy as np
 # Setting default random generator
 rng = np.random.default_rng(12345)
 
-def get_square_matrix(m, n, random=False):
-    '''Return a square matrix with integer scalaras. Whether random or not'''
+def get_square_matrix(m, n, random=False, integer=1):
+    '''Return a square matrix with integer scalaras. Whether random or specified'''
     if m != n:
         raise ValueError(f"Rows and columns numbers don't match: {m} != {n}.")
 
     if random:
         return rng.integers(50, size=(m, n))
     else:
-        return np.ones((m, n), dtype=int)
+        return np.ones((m, n), dtype=int) * integer
 
-m = """
-Square matrices are a type of matrices where column and row are equal.
-(since it's a square Duh). These types of matrices allow to be used in
-matrix arithmetic with ease due to equal sizes."""
+
+if __name__ == "__main__":
+    # Square matrix
+    m = """
+    Square matrices are a type of matrices where column and row are equal.
+    (since it's a square Duh). These types of matrices allow to be used in
+    matrix arithmetic with ease due to equal sizes."""
+    print(m.strip())
+    print('4 x 4 square matrix')
+    print(get_square_matrix(4, 4, True))
+    print("Let's do some square matrix arithmetic")
+    A = get_square_matrix(5, 5, integer=2)
+    B = get_square_matrix(5, 5, integer=5)
+    print('A:\n', A)
+    print('B:\n', B)
+    print("Let's do some multiplication and division in the form of A * B and A / B")
+    print("Multiplication:\n", A * B)
+    print("Division:\n", A / B)
+    print("Keep in mind that it multiplies across elements, not dot products")
+
