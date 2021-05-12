c65b735d512384c341dd6a85344faaa37802bcd3
pavelexpertov <pavelm01@hotmail.co.uk>
2021-05-12 08:14:36 +0100

    Add symmetrical matrix

--- linear_algebra_by_j_borwnlee/ch_10/scribble_of_different_types_of_matrices.py
+++ linear_algebra_by_j_borwnlee/ch_10/scribble_of_different_types_of_matrices.py
@@ -22,7 +22,18 @@ def get_symmetrical_matrix(n):
         permutations.append(current_integer_list[:])
         current_integer_list.pop()
         current_integer_list.insert(0, current_integer_list[0] + 1)
+    # To add the last row
+    permutations.append(current_integer_list[:])
     return np.array(permutations)
+
+def format_message_str(string):
+    '''Return re-formated string for output messages.
+
+    It's expected a docstring with 4 spaces that delimits sentences
+    with some new lines at each end.
+    '''
+    string = string.strip()
+    return "\n".join(string.split(" "*4))
 
 if __name__ == "__main__":
     # Square matrix
@@ -30,7 +41,7 @@ if __name__ == "__main__":
     Square matrices are a type of matrices where column and row are equal.
     (since it's a square Duh). These types of matrices allow to be used in
     matrix arithmetic with ease due to equal sizes."""
-    print(m.strip())
+    print(format_message_str(m))
     print('4 x 4 square matrix')
     print(get_square_matrix(4, 4, True))
     print("Let's do some square matrix arithmetic")
@@ -42,4 +53,19 @@ if __name__ == "__main__":
     print("Multiplication:\n", A * B)
     print("Division:\n", A / B)
     print("Keep in mind that it multiplies across elements, not dot products")
-    print("\n", get_symmetrical_matrix(5))
+    # Symmetrical matrix
+    m = """
+    Symmetrical matrix is a square matrix that has values mirrored along a diagonal line.
+    The diagonal line consists of just '1' scalars and then each scalar get's incremented to left and right
+    sides of the diagonal line.
+    For example, a matrix with 16 dimension size would look like this:
+    """
+    print(format_message_str(m))
+    print("\n", get_symmetrical_matrix(16))
+    print("It has to be a square such that the values can be mirrored")
+
+    # Triangular matrix
+    
+    # Diagonal matrix
+    # Identity matrix
+    # Orthogonal matrix
