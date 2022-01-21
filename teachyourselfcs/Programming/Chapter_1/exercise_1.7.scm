fa7c3ca94f9b06f5d6c5372a116496508d5d96d1
pavelexpertov <pavelm01@hotmail.co.uk>
2022-01-21 23:32:30 +0000

    some work

---
+++ teachyourselfcs/Programming/Chapter_1/exercise_1.7.scm
@@ -0,0 +1,72 @@
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x)
+                 x)))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (square x)
+		(* x x))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+;This is output from performing the square root calculation:
+;1 ]=> (sqrt 0.001)
+
+;Value: .04124542607499115
+
+;1 ]=> (sqrt 0.00001)
+
+;Value: .03135649010771716
+
+;1 ]=> (sqrt 0.000000001)
+
+;Value: .03125001065624928
+
+;1 ]=> (sqrt 0.0000000000000001)
+
+;Value: .03125000000000106
+
+;1 ]=> (sqrt 0.00000000000000000000001)
+
+;Value: .03125
+
+;1 ]=> (sqrt 0.0000000000000000000000000001)
+
+;Value: .03125
+
+;1 ]=> (sqrt-iter 0.1 0.001)
+
+;Value: .03659090909090909
+
+;1 ]=> (sqrt-iter 0.1 0.00001)
+
+;Value: .0251249000999001
+
+;1 ]=> (sqrt-iter 0.1 0.000000001)
+
+;Value: .025000012499999
+
+;1 ]=> (sqrt-iter 0.1 0.0000000000000001)
+
+;Value: .02500000000000125
+
+;1 ]=> (sqrt 0.1 0.00000000000000000000001)
+
+;1 ]=> (sqrt-iter 0.1 0.00000000000000000000001)
+
+;Value: .025
+
+;]=> (sqrt-iter 0.1 0.0000000000000000000000000001)
+
+;Value: .025
+
--- teachyourselfcs/Programming/README.md
+++ teachyourselfcs/Programming/README.md
@@ -8,8 +8,8 @@ Make sure you got the script of `mit-scheme.sh`
 Then run `./mit-scheme.sh`
 
 ## How to load files into scheme REPL?
-Use this procedure and replace the absolute path with your one (i.e. Basically use `pwd` command and expand '~' as necessary). Example:
+Use this procedure and replace the absolute path with your one (i.e. Basically use `pwd` command and/or expand '~' as necessary). Example:
 ```scheme
 (load "/home/pavel/learning/Programming/Chapter_1/exercise_1.3.scm")
 ```
-
+**Note**: If you type `echo $(pwd)/exercise.1.7.scm` and use __Tab__ key after just the closing round bracket, that should ease the process of generating abosulte paths easily.
