f1169ba88e33a2d7d43e5a49e7224f18ecb69a62
pavelexpertov <pavelm01@hotmail.co.uk>
2022-01-24 10:33:04 +0000

    Finally completed 1.7 exercise

--- teachyourselfcs/Programming/Chapter_1/exercise_1.7.scm
+++ teachyourselfcs/Programming/Chapter_1/exercise_1.7.scm
@@ -5,13 +5,18 @@
                  x)))
 
 (define (improve guess x)
-  (average guess (/ x guess)))
+        (average guess (/ x guess)))
 
 (define (average x y)
-  (/ (+ x y) 2))
+        (/ (+ x y) 2))
 
 (define (good-enough? guess x)
-  (< (abs (- (square guess) x)) 0.001))
+        (< (abs (- (square guess) x)) 0.001))
+
+;Improved good-enough? test
+(define (good-enough? guess x)
+        (< (abs (- guess (improve guess x)))
+           0.00000001))
 
 (define (square x)
 		(* x x))
@@ -40,99 +45,124 @@
 
 ; Look at the examples below:
 ;-----------------------------------
-1 ]=> (sqrt (multiply-tenth 6))
+;1 ]=> (sqrt (multiply-tenth 6))
 
 ;Value: 3.1260655525445276e-2
 
-1 ]=> (sqrt (multiply-tenth 7))
+;1 ]=> (sqrt (multiply-tenth 7))
 
 ;Value: .03125106561775382
 
-1 ]=> (sqrt (multiply-tenth 8))
+;1 ]=> (sqrt (multiply-tenth 8))
 
 ;Value: .03125010656242753
 ;-----------------------------------
-It can be observed that the change in decimal value (100 thousandth place)
-doesn't change because it procedure's condition will be satisfied
-(i.e. 0.03125010656242753^2 - 0.000000001 < 0.001).
-However, the calculator will show that:
-square root of 0.00000001 will be 0.0001 and square root of 0.000000001
-will be 0.000031623. The decimal value is not wrong but it's the decimal
-places that the good-enough? test couldn't 
+;It can be observed that the change in decimal value (100 thousandth place)
+;doesn't change because it procedure's condition will be satisfied
+;(i.e. 0.03125010656242753^2 - 0.000000001 < 0.001).
+;However, the calculator will show that:
+;square root of 0.00000001 will be 0.0001 and square root of 0.000000001
+;will be 0.000031623. The decimal value is not wrong but it's the decimal
+;places that the good-enough? test couldn't 
 
 
 ;As for very large numbers, the good-enough? test will not suffice because
 ;when calculating differences between squared guess and the radicand, the
 ;condition that tests for closeness of both numbers doesn't get satisfied
-;because the differences are very.
+;because the differences are very big.
 ;If you look below, 
-
-
-; Output for testing large numbers
-
-1 ]=> (sqrt (multiply-tens 8))
-
-;Value: 10000.
-
-1 ]=> (sqrt (multiply-tens 9))
-
-;Value: 31622.776601684047
-
-1 ]=> (sqrt (multiply-tens 10))
-
-;Value: 100000.
-
-1 ]=> (sqrt (multiply-tens 11))
+;-----------------------------------------
+;1 ]=> (sqrt (multiply-tens 11))
 
 ;Value: 316227.7660168379
 
-1 ]=> (sqrt (multiply-tens 12))
+;1 ]=> (sqrt (multiply-tens 12))
 
 ;Value: 1000000.
 
-1 ]=> (sqrt (multiply-tens 13))
+;1 ]=> (sqrt (multiply-tens 13))
+;-----------------------------------------
+;It can be observed that there's a limit of calcuating the square roots
+;becuase when a number contains 13 zeros, the procedure struggles to
+;calculate it by being stuck in an endless loop. This is caused not
+;satisfying the aformentioned condition.
+;For example, actual square root value is 3162277.660168379.
+;If we give it to the good-enough? test, you get the following:
+;----------------------------------------------------------------
+;1 ]=> (good-enough? 3162277.660168379 (multiply-tens 13))
+
+;Value: #f
+
+;1 ]=> (good-enough? 1000000 (multiply-tens 12))
+
+;Value: #t
+;----------------------------------------------------------------
+
+; Output for testing large numbers
+
+;1 ]=> (sqrt (multiply-tens 8))
+
+;Value: 10000.
+
+;1 ]=> (sqrt (multiply-tens 9))
+
+;Value: 31622.776601684047
+
+;1 ]=> (sqrt (multiply-tens 10))
+
+;Value: 100000.
+
+;1 ]=> (sqrt (multiply-tens 11))
+
+;Value: 316227.7660168379
+
+;1 ]=> (sqrt (multiply-tens 12))
+
+;Value: 1000000.
+
+;1 ]=> (sqrt (multiply-tens 13))
 
 
 
 ; Output for testing small numbers
 
-1 ]=> (sqrt (multiply-tenth 1))
+;1 ]=> (sqrt (multiply-tenth 1))
 
 ;Value: .316245562280389
 
-1 ]=> (sqrt (multiply-tenth 2))
+;1 ]=> (sqrt (multiply-tenth 2))
 
 ;Value: .10032578510960607
 
-1 ]=> (sqrt (multiply-tenth 3))
+;1 ]=> (sqrt (multiply-tenth 3))
 
 ;Value: .04124542607499115
 
-1 ]=> (sqrt (multiply-tenth 4))
+;1 ]=> (sqrt (multiply-tenth 4))
 
 ;Value: .03230844833048122
 
-1 ]=> (sqrt (multiply-tenth 5))
+;1 ]=> (sqrt (multiply-tenth 5))
 
 ;Value: .03135649010771716
 
-1 ]=> (sqrt (multiply-tenth 6))
+;1 ]=> (sqrt (multiply-tenth 6))
 
 ;Value: 3.1260655525445276e-2
 
-1 ]=> (sqrt (multiply-tenth 7))
+;1 ]=> (sqrt (multiply-tenth 7))
 
 ;Value: .03125106561775382
 
-1 ]=> (sqrt (multiply-tenth 8))
+;1 ]=> (sqrt (multiply-tenth 8))
 
 ;Value: .03125010656242753
 
-1 ]=> (sqrt (multiply-tenth 9))
+;1 ]=> (sqrt (multiply-tenth 9))
 
 ;Value: .03125001065624928
 
-1 ]=> (sqrt (multiply-tenth 10))
+;1 ]=> (sqrt (multiply-tenth 10))
 
 ;Value: .03125000106562499
 
